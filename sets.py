6f850e45c5e797c56f76c51f9b4c39bbf15f2c50
Sudhanshu <sudhanshu.s>
2020-12-19 16:13:56 +0530

    added sets.py

---
+++ sets.py
@@ -0,0 +1,13 @@
+#Sets is unordered collect of elements
+my_set=set()
+print(my_set)
+my_set.add(1)
+print(my_set)
+my_set.add(2)
+print(my_set)
+my_set.add(2)
+print(my_set)
+
+#print unique values from a list using set function
+l=[1,2,3,4,5,6,1,2,3,4,5,6]
+print(set(l))
---
+++ tuples.py
@@ -0,0 +1,17 @@
+t=(1,2,3,3,4,4)
+l=[1,2,3,4,4]
+print(t[2])
+print(l[2])
+t1=([1,2,3],4,[6,7,8])
+print(t1)
+print(t.count(3))
+print(t.count(4))
+print(l.count(4))
+
+l[0]=10
+print(l)
+
+print(t.index(4))
+
+t[0]=10
+print(t)
