2e7f5e992471eef1d53a00f4f006ddd7516fae5f
Sudhanshu <sudhanshu.s>
2020-12-19 15:19:35 +0530

    Added string.py

--- list.py
+++ list.py
@@ -2,7 +2,27 @@
 a=[3,1,2,4,2,3,6,2]
 a.sort()
 print(a)
+print(type(a))
+
+#Reverse a list
+b=['l','m','a','b','c']
+b.reverse()
+print(b)
+
+#Append to list
+b.append('o')
+print(b)
+print('Length of b: ',len(b))
+
+b.extend(b[0:5])
+print(b)
+print('Length of b: ',len(b))
+
 #Sort a list with second element of tuple
-a=[('abc',8),('l',6),('lmn',10)]
-a.sort(key=lambda x: x[1])
-print(a)
+c=[('abc',8),('l',6),('lmn',10)]
+c.sort(key=lambda x: x[1])
+print(c)
+print(type(c[0]))
+
+info=help(c)
+print(info)
---
+++ string.py
@@ -0,0 +1,22 @@
+name="Sudhanshu Sinha"
+
+#Print range from string
+print(name[0:10])
+print(name[10:15])
+print(name[5:-1])
+
+#Print range from string with steps of 2
+print(name[1:-1:2])
+
+#Reverse a String
+print(name[::-1])
+print(name.upper())
+
+#Split a string into list with seperator 'h'
+print(name.split('h'))
+
+#String Formatting
+print("My name is {}".format(name))
+
+result=1222.1023054
+print("The result is {r:0.5f}".format(r=result))
