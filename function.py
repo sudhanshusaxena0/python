2ddbd2f4c33031361c2ff5b702ca6107ac150274
Sudhanshu <sudhanshu.s>
2020-12-20 13:08:37 +0530

    added

--- function.py
+++ function.py
@@ -75,3 +75,66 @@ def check_costly_fruit(fruits):
 print(check_costly_fruit(prices))
 fruit,cost=check_costly_fruit(prices)
 print(f"{fruit} cost is {cost}")
+
+example=[1,2,3,4,5,6,7]
+from random import shuffle
+shuffle(example)
+print(example)
+
+my_list=['0',' ',' ']
+print(my_list)
+
+def shuffle_mylist(mylist):
+    shuffle(mylist)
+    return mylist
+print(shuffle_mylist(my_list))
+
+def player_guess():
+    guess=''
+    while guess not in ['0','1','2']:
+        guess=input("Pick a number: 0,1, or 2: ")
+    return int(guess)
+
+player_guess()
+
+def check_guess(mylist,guess):
+    if mylist[guess] == '0':
+        print("Correct!")
+    else:
+        print("Wrong guess!")
+        print(mylist)
+
+check_guess(my_list,player_guess())
+
+#arbiterary number of parameters with * and saves the parameters as tuple
+def add_num(*nums):
+    print(nums)
+    return sum(nums)
+
+print(add_num(1,2,3,4))
+
+#arbitrary number of key vale pairs with ** and save the parameters as dictionary
+def myfunc(**dict1):
+    print(dict1)
+    if 'fruit' in dict1:
+        print(f"My fruit of choice is {dict1['fruit']}")
+
+myfunc(fruit="apple",veggie="beans")
+
+def myfunc1(*args,**kwargs):
+    print(args)
+    print(kwargs)
+    print(f"I would like {args[0]} {kwargs['food']}")
+myfunc1(10,20,30,fruit="orange",food="eggs",animal="dog")
+
+def upper_lower(string1):
+    case_string=""
+    for i in string1:
+        if i%2 == 0:
+            case_string=case_string+i.upper()
+        else:
+            case_string=case_string+i.lower()
+
+    return case_string
+
+upper_lower("sudhanshu")
---
+++ test.py
@@ -0,0 +1,7 @@
+a='a'
+b='b'
+c=''
+c=c+a.upper()+b.upper()
+print(c)
+
+help(str)
